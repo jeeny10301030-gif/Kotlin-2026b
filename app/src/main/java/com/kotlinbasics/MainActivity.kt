128b97609b65be6cc06d4f6e1408bcd7892e6d4b
jimin <jeeny10301030@gmail.com>
2026-09-14 12:25:51 +0900

    kotlin ëfunction

--- app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -71,6 +71,31 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+private fun week02Functions(){
+//    println("Week 02: Functions")
+//
+//    fun greet(name: String) = "Hello, $name!"
+//
+//    println(greet("Android developer"))
+
+    println("!!== Kotlin Functions ==!!")
+
+    fun greet(name: String): String {
+        return "Hello, $name!"
+    }
+
+    fun add(a: Int, b: Int) = a + b
+
+    fun introduce(name: String, age: Int = 19){
+        println("My name is $name and I'm $age years old")
+    }
+
+    println(greet("Kotlin"))
+    println("Sum: ${add(5, -71)}")
+    introduce("Kim", 7)
+    introduce("Park")
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
